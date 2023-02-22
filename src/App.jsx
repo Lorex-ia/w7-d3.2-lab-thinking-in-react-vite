ffb2922f08fede6a91c52ec571e5af1d61a71e52
Lorexia <alexia.lorenza.martinel@protonmail.com>
2023-02-22 20:52:59 +0100

    just starting

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,27 @@
-import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+// src/App.js
+import "./App.css";
+import goods from './data.json'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log(goods)
 
-  return (
-    <div className='App'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </div>
+  return(
+    
+    <div className="App">
+
+
+
+
+
+
+
+
+
+
+
+  </div>
   )
 }
-
-export default App
+export default App;
